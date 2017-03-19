42c4f058e14ce16cb9ac04c89554e5d0d7432d4d
alexpts <alexpts@yandex.ru>
2017-03-19 13:22:29 +0300

    InArray validator use strict mode always

--- src/PTS/Validator/Validators/InArrayValidator.php
+++ src/PTS/Validator/Validators/InArrayValidator.php
@@ -3,8 +3,8 @@ namespace PTS\Validator\Validators;
 
 class InArrayValidator
 {
-    public function __invoke($value, array $allow, bool $strict = true): bool
+    public function __invoke($value, array $allow): bool
     {
-        return in_array($value, $allow, $strict);
+        return in_array($value, $allow, true);
     }
 }
