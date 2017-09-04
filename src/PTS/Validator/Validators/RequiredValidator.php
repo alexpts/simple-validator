e79946b2737d9835a82043225f4bc57b1f462615
alexpts <alexpts@yandex.ru>
2017-09-04 20:40:57 +0300

    refactoring

--- src/PTS/Validator/Validators/RequiredValidator.php
+++ src/PTS/Validator/Validators/RequiredValidator.php
@@ -7,14 +7,20 @@ class RequiredValidator
 {
     public function __invoke($value): bool
     {
-        if ($value === null) {
+        if ($value === null || $this->validateString($value)) {
             return false;
         }
 
-        if (is_string($value) && trim($value) === '') {
-            return false;
-        }
+        return !$this->validateCountable($value);
+    }
 
-        return !((is_array($value) || $value instanceof \Countable) && count($value) < 1);
+    protected function validateString($value): bool
+    {
+        return is_string($value) && trim($value) === '';
+    }
+
+    protected function validateCountable($value): bool
+    {
+        return (is_array($value) || $value instanceof \Countable) && count($value) < 1;
     }
 }
